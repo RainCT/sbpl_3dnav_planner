1614ecf3aa28348a53b9ca8e051fd2fb02b17417
Andrew Dornbush <andrew.dornbush@gmail.com>
2012-03-01 16:18:14 -0500

    included pose_follower_3d package with upgraded to ros-electric

---
+++ pose_follower_3d/build/CMakeFiles/pose_follower_3d.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/andrew/ros_electric_workspace/3d_navigation/pose_follower_3d/src/pose_follower_3d.cpp" "/home/andrew/ros_electric_workspace/3d_navigation/pose_follower_3d/build/CMakeFiles/pose_follower_3d.dir/src/pose_follower_3d.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ pose_follower_3d/build/CMakeFiles/pose_follower_3d.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pose_follower_3d.dir/src/pose_follower_3d.o"
+  "../lib/libpose_follower_3d.pdb"
+  "../lib/libpose_follower_3d.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pose_follower_3d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
